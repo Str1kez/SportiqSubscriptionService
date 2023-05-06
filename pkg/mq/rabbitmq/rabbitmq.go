b677dca34f39cb239befbe95aff67805d387f58b
Str1kez <vazinov@yandex.ru>
2023-05-06 04:13:19 +0300

    🔨 added: structure reworked

---
+++ pkg/mq/rabbitmq/rabbitmq.go
@@ -0,0 +1,59 @@
+package rabbitmq
+
+import (
+	"fmt"
+
+	"github.com/Str1kez/SportiqSubscriptionService/internal/config"
+	amqp "github.com/rabbitmq/amqp091-go"
+	log "github.com/sirupsen/logrus"
+)
+
+type RabbitMQ struct {
+	connection *amqp.Connection
+	channel    *amqp.Channel
+	queue      *amqp.Queue
+}
+
+func NewRabbitMQ(config *config.MQConfig) *RabbitMQ {
+	connection, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", config.User, config.Password, config.Host, config.Port))
+	if err != nil {
+		log.Fatalf("Couldn't connect to RabbitMQ: %v\n", err)
+	}
+	return &RabbitMQ{connection: connection}
+}
+
+func (r *RabbitMQ) OpenChannel() {
+	channel, err := r.connection.Channel()
+	if err != nil {
+		log.Panicf("Couldn't open channel: %v\n", err)
+	}
+	r.channel = channel
+}
+
+func (r *RabbitMQ) DeclareQueue(name string, durable bool, prefetchCount int) {
+	queue, err := r.channel.QueueDeclare(name, durable, false, false, false, nil)
+	if err != nil {
+		log.Panicf("Couldn't declare queue [%s]: %v\n", name, err)
+	}
+	err = r.channel.Qos(prefetchCount, 0, false)
+	if err != nil {
+		log.Panicf("Couldn't declare params for queue [%s]: %v\n", name, err)
+	}
+	r.queue = &queue
+}
+
+func (r *RabbitMQ) Close() error {
+	if r.channel != nil && !r.channel.IsClosed() {
+		err := r.channel.Close()
+		if err != nil {
+			return err
+		}
+	}
+	if r.connection != nil && !r.connection.IsClosed() {
+		err := r.connection.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
